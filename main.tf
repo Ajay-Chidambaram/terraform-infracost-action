fef19497b6fc00fcdeaa8dc71f0ebe955f97af77
chidambaram k <chidambaramk@chidambarams-MacBook-Pro.local>
2024-07-28 15:35:05 +0530

    reusable workflow test

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "random_uuid" "random_id" {}
+
+module "webapp" {
+  source = "./modules/web-app"
+
+  instance_type                 = "t3.small"
+  root_block_device_volume_size = 100
+  block_device_volume_size      = 250
+  block_device_iops             = 600
+
+  hello_world_function_memory_size = 128
+
+  tags = {
+    ApplicationOwner = "Kavin",
+    FinancialOwner   = "Kavin",
+    createdBy        = "Terraform",
+    env              = "sandbox"
+  }
+}
